7d0de066566737a09953dbf69c2643dfa5bca194
Albert Johanson <aj.dev.gt@gmail.com>
2021-03-19 15:53:58 -0600

    Finish the nav

--- src/Components/BottomNavigation/BottomNav.jsx
+++ src/Components/BottomNavigation/BottomNav.jsx
@@ -1,33 +1,45 @@
-import React from 'react'
+import React from "react";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faSearch,
+  faDownload,
+  faPlay,
+} from "@fortawesome/free-solid-svg-icons";
 
 /* Style */
-import '../../assets/style/Components/BottomNav.sass'
-
-
+import "../../assets/style/Components/BottomNav.sass";
 
 function BottomNav() {
-    return (
-        <div className="nav__container">
-            <div className="nav__container--tab">
-                <div className="nav__container--link">
-                    <FontAwesomeIcon icon={faHome} size="2x"/>
-                    <p>Inicio</p>
-                </div>
-            </div>
-            <div className="nav__container--tab">
-                <p>Proximamente</p>
-            </div>
-            <div className="nav__container--tab">
-                <p>Buscar</p>
-            </div>
-            <div className="nav__container--tab">
-                <p>Descargas</p>
-            </div>
+  return (
+    <div className="nav__container">
+      <div className="nav__container--tab">
+        <div className="nav__container--link">
+          <FontAwesomeIcon icon={faHome} size="2x" color="white"/>
+          <p>Inicio</p>
         </div>
-    )
+      </div>
+      <div className="nav__container--tab">
+      <div className="nav__container--link">
+          <FontAwesomeIcon icon={faPlay} size="2x" color="white"/>
+          <p>Proximamente</p>
+        </div>
+      </div>
+      <div className="nav__container--tab">
+        <div className="nav__container--link">
+          <FontAwesomeIcon icon={faSearch} size="2x" color="white"/>
+          <p>Buscar</p>
+        </div>
+      </div>
+      <div className="nav__container--tab">
+        <div className="nav__container--link">
+          <FontAwesomeIcon icon={faDownload} size="2x" color="white"/>
+          <p>Descargas</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default BottomNav
+export default BottomNav;
